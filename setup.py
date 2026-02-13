90420ad6ecb2a6fabf5bedfe3d1cf0e5f8c014a3
Maksym Kozlenko <max@kozlenko.info>
2026-02-14 10:56:06 +1100

    modified setup

--- setup.py
+++ setup.py
@@ -9,17 +9,17 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="m-hass-api",
-    version="0.3.0",
+    version="0.4.0",
     author="Maksym Kozlenko",
     author_email="max@kozlenko.info",
-    description="Provide access to Home Assistant API",
+    description="Python package for Home Assistant API with real-time WebSocket state monitoring",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/maxim75/m-hass-api",
     project_urls={
-        "Bug Tracker": "https://github.com/yourusername/m-hass-api/issues",
-        "Documentation": "https://github.com/yourusername/m-hass-api/blob/main/README.md",
-        "Source Code": "https://github.com/yourusername/m-hass-api",
+        "Bug Tracker": "https://github.com/maxim75/m-hass-api/issues",
+        "Documentation": "https://github.com/maxim75/m-hass-api/blob/main/README.md",
+        "Source Code": "https://github.com/maxim75/m-hass-api",
     },
     packages=find_packages(exclude=["tests", "tests.*"]),
     classifiers=[
@@ -38,6 +38,9 @@ setup(
     python_requires=">=3.8",
     install_requires=[
         "requests>=2.25.0",
+        "websocket-client>=1.0.0",
+        "python-dotenv>=0.19.0",
+        "pandas>=1.3.0",
     ],
     extras_require={
         "dev": [
@@ -48,10 +51,10 @@ setup(
             "mypy>=0.950",
         ],
     },
-    keywords="api client template sample",
+    keywords="home-assistant api websocket rest client home automation",
     entry_points={
         "console_scripts": [
-            "m-hass-api=m_hass_api.sample:main",
+            "m-hass-api=m_hass_api.run:main",
         ],
     },
 )
